d7e82d4dac79045337b7ea2bde4433e35fa64cdd
Vladyslav Hryhorovych <vladwwe24@gmail.com>
2022-10-10 23:02:12 +0300

    Connect to SqLite DB and create Session

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 const path = require("path");
-const sqlite3 = require('sqlite3').verbose();
-const {MemorySession} = require('telegram/sessions');
+const sqlite3 = require("sqlite3");
+const {open} = require('sqlite');
+const {AuthKey} = require("telegram/crypto/AuthKey");
+const {MemorySession} = require("telegram/sessions");
 
 const EXTENSION = '.session'
 
 class SQLiteSession extends MemorySession {
     constructor(sessionPath, extension = EXTENSION) {
         super();
+        sqlite3.verbose();
+
         this.fileName = ':memory:';
 
-        if(sessionPath){
+        if (sessionPath) {
             this.fileName = path.extname(sessionPath) === extension ? sessionPath : sessionPath + extension;
         }
     }
 
-    async load(){
-        new sqlite3.Database(this.fileName, sqlite3.OPEN_READONLY, (err) => {
-            if(err){
-                console.error(err);
-            }
-            console.log("Connected to " + this.fileName);
+    async load() {
+        this._conn = await open({
+            filename: this.fileName,
+            driver: sqlite3.Database
         });
+        const session = await this._conn.get("select * from sessions");
+
+        let authKey = session?.auth_key;
+        if (authKey && typeof authKey === "object") {
+            this._authKey = new AuthKey();
+            if ("data" in authKey) {
+                authKey = Buffer.from(authKey.data);
+            }
+            await this._authKey.setKey(authKey);
+        }
+
+        const dcId = session?.dc_id;
+        if (dcId) {
+            this._dcId = dcId;
+        }
+
+        const port = session?.port;
+        if (port) {
+            this._port = port;
+        }
+        const serverAddress = session?.server_address;
+        if (serverAddress) {
+            this._serverAddress = serverAddress;
+        }
     }
 }
 
